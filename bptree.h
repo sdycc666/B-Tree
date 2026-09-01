b9a56cc10a0500b68a83a7c4277878dff97e3104
sdy <2021514896@qq.com>
2026-09-01 22:29:28 +0800

    使用mmap实现了持久化b+树

--- bptree.h
+++ bptree.h
@@ -1,42 +1,47 @@
 #ifndef BPTREE_H
 #define BPTREE_H
 
-#include <stdlib.h>
+#include <stddef.h>
+#include <sys/types.h>
 
-#define MAX 4
-#define MY_INT_MAX 0x7FFFFFFF // 自定义int最大值，把未使用的空位都填充成这个最大值，用来标记
+#define ORDER 4                // B+树阶数：叶子最多存4个key，非叶子最多存3个key
+#define PAGE_SIZE 4096         // 页大小（第一页存文件头）
+#define MAGIC 0x42505452       // 文件魔数 "BPTR"
+#define INIT_FILE_SIZE (16 * 1024 * 1024)  // 预分配16MB
 
-// 非叶子节点
-typedef struct NNode
-{
-    int key[MAX - 1];
-    int keynum;
-    void *child[MAX];
-} NNode;
-
-// 叶子节点
-typedef struct LNode
-{
-    int data[MAX];
-    int datanum;
-    struct LNode *next;
-    struct LNode *prev;
-} LNode;
-
-// 树结构
-typedef struct BPtree
-{
-    void *root;
-    LNode *leaf_head;
-    LNode *leaf_tail;
+typedef struct {
+    int magic;
+    int page_size;
+    off_t root;
     int height;
-} BPtree;
+    off_t next_free;
+} Header;
 
-void init(BPtree *B);
-void insert(BPtree *B, int data);
-int search(BPtree *B, int data);
-void range_search(BPtree *B, int start, int end);
-void show(BPtree *B);
-void destroy(BPtree *B);
+typedef struct {
+    int is_leaf;
+    int count;
+    int key[ORDER];
+    off_t child[ORDER + 1];
+} Node;
 
-#endif // BPTREE_H
+
+typedef struct {
+    int fd;
+    void *base;
+    size_t file_size;
+} BPTree;
+
+// 打开/创建 B+树文件（内部做 mmap 映射）
+BPTree *bptree_open(const char *path);
+// 关闭：同步写盘 + 解除映射 + 释放资源
+void bptree_close(BPTree *tree);
+// 插入一个整数，重复值忽略
+int bptree_insert(BPTree *tree, int key);
+// 单点查找，存在返回1，不存在返回0
+int bptree_search(BPTree *tree, int key);
+// 区间查找 [start, end]，打印结果
+void bptree_range_search(BPTree *tree, int start, int end);
+// 沿叶子链表打印全部数据
+void bptree_show(BPTree *tree);
+
+#endif
